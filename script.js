4e08cb7fa031a4061f71943916286b6f4c6a887c
Taniesha Robinson <taniesha@uchicago.edu>
2024-05-15 14:53:11 -0500

    another loop through links

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-let navLinks = document.querySelector(".nav-link");
+let navLinks = document.querySelectorAll(".nav-link");
 let courseFinder = document.querySelector(".course-finder");
 
-navLinks.addEventListener("click", function(){
-  let contentSections = document.getElementById("main").children;
+for (let i = 0; i < navLinks.length; i++) {
+  const element = navLinks[i];
 
-  for (let index = 0; index < contentSections.length; index++) {
-    const element = contentSections[index];
-    element.style.display = "none";
-  }
+  element.addEventListener("click", function () {
+    let contentSections = document.getElementById("main").children;
 
-});
+    for (let i = 0; i < contentSections.length; i++) {
+      const element = contentSections[i];
+      element.style.display = "none";
+    }
+
+  });
+
+}
+
+
 
 courseFinder.addEventListener("click", function(){
   document.getElementById("course-content").style.display = "block";
