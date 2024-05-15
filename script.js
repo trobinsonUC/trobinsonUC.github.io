192b5638e7657ba53d7a59ec33ffab892f4b8dac
Taniesha Robinson <taniesha@uchicago.edu>
2024-05-15 14:30:30 -0500

    update nav functionality

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function showCourses(){
+let navLinks = document.querySelector(".nav-link");
+let courseFinder = document.querySelector(".course-finder");
+
+navLinks.addEventListener("click", function(){
+  document.getElementById("main").children.style.display = "none";
+});
+
+courseFinder.addEventListener("click", function(){
   document.getElementById('course-content').style.display = "block";
-}
+});
